3ff821b16b765e363ffa77d2dc0018ed3b600b57
Masrafi Bin Seraj <masrafi190116@gmail.com>
2024-02-23 11:47:32 +0600

    Minor Fixes

--- script.js
+++ script.js
@@ -12,7 +12,14 @@ function windowOnClick(event) {
     }
 }
 
-
+function topnavToggle() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 trigger.addEventListener("click", toggleModal);
 closeButton.addEventListener("click", toggleModal);
